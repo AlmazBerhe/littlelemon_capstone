6d8362923cb60685f59ae9162b8fdf9617f642f8
AlmazBerhe <zamlatak@gmail.com>
2024-05-21 17:08:48 +0100

    test

---
+++ restaurant/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+# Test driven django project
+
+class InitialTest(TestCase):
+    def setup(self):
+        pass
+    
+    def test_model_creation(self):
+        pass
