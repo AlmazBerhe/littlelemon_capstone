b1a95ce92475364785b3b3e9b8e6a0c5ed0d8d6b
AlmazBerhe <zamlatak@gmail.com>
2024-05-23 15:24:44 +0100

    models, database setup and migrations to mysql database

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -74,12 +74,10 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 
 # Password validation
