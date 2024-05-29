6103ede04192d906d507a41f2ffb93f95d0c047c
AlmazBerhe <zamlatak@gmail.com>
2024-05-29 15:03:37 +0100

    added unit testing for Menu model and view

--- restaurant/models.py
+++ restaurant/models.py
@@ -9,3 +9,9 @@ class Menu(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.IntegerField()
+    
+    def __str__(self):
+        return f'{self.title} : {str(self.price)}'
+    
+    def get_item(self):
+        return f'{self.title} : {str(self.price)}'
---
+++ restaurant/tests/test_views.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+from restaurant.models import Menu
+
+class MenuViewTest(TestCase):
+    def setUp(self):
+       Menu.objects.create(title="Banana shake", price=10, inventory=20)
+       Menu.objects.create(title="Peanut butter shake", price=12, inventory=20)
+       Menu.objects.create(title="Smoothie", price=6.77, inventory=20)
+       Menu.objects.create(title="Milkshake", price=4.5, inventory=20)
+       
+    
+    def test_getall(self):
+        items = Menu.objects.all()
+        self.assertEqual(items.count(), 4)
+        
+        expected_values = [
+            "Banana shake : 10.00",
+            "Peanut butter shake : 12.00",
+            "Smoothie : 6.77",
+            "Milkshake : 4.50",
+        ]
+        
+        item_list = [f"{item.title} : {str(item.price)}" for item in items]
+        
+        self.assertListEqual(item_list, expected_values)
+        
+        
