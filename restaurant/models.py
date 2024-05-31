19a4e7170b01950ebed9bdc0ca3368603bf45225
AlmazBerhe <zamlatak@gmail.com>
2024-05-31 15:25:21 +0100

    changed model attributes

--- restaurant/models.py
+++ restaurant/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class Booking(models.Model):
     name = models.CharField(max_length=255)
     no_of_guests = models.IntegerField()
-    bookingDate = models.DateTimeField()
+    reservation_date = models.DateField()
+    reservation_slot = models.SmallIntegerField(default=10)
     
 class Menu(models.Model):
     title = models.CharField(max_length=255)
